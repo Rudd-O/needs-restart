d9dafdbff3bc6a082f238e06700228a3f52e8207
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2015-10-25 10:07:32 +0000

    build and deploy files included

---
+++ setup.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import os
+
+dir = os.path.dirname(__file__)
+path_to_readme = os.path.join(dir, "README.md")
+version = "0.0.1"
+readme = open(path_to_readme).read(-1)
+
+classifiers = [
+'Development Status :: 5 - Production/Stable',
+'Environment :: Console',
+'Environment :: No Input/Output (Daemon)',
+'Intended Audience :: End Users/Desktop',
+'Intended Audience :: System Administrators',
+'License :: OSI Approved :: GNU General Public License (GPL)',
+'Operating System :: POSIX :: Linux',
+'Programming Language :: Python :: 2 :: Only',
+'Programming Language :: Python :: 2.7',
+'Topic :: Utilities',
+]
+
+setup(
+	name = 'needs-restarting',
+	version=version,
+	description = 'A program to discover services that need restarting',
+	long_description = readme,
+	author='Manuel Amador (Rudd-O)',
+	author_email='rudd-o@rudd-o.com',
+	license="GPL",
+	url = 'http://github.com/Rudd-O/needs-restarting',
+	package_dir=dict(),
+	classifiers = classifiers,
+	scripts = ["needs-restarting"],
+	keywords = "systemd services",
+	zip_safe=False,
+)
